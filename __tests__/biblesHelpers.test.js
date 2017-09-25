5e62df362d4e3884e1156459a124852d51c12993
Joel Lonbeck <joel@neutrinographics.com>
2017-09-25 15:12:52 -0700

    stubbed out tests

--- __tests__/biblesHelpers.test.js
+++ __tests__/biblesHelpers.test.js
@@ -1,7 +1,11 @@
 import { generateBibles, getResourceManifestFromYaml } from "../helpers/biblesHelpers";
 
 describe('Bible Helpers', () => {
-    test('generateBibles', () => {
+    test('generateBibles with good input', () => {
+        expect(false).toBeTruthy();
+    });
 
+    test('generateBibles with bad input', () => {
+        expect(false).toBeTruthy();
     });
 });
---
+++ __tests__/door43ApiHelper.test.js
@@ -0,0 +1,11 @@
+import { getUrl } from "../helpers/door43ApiHelper";
+
+describe('Door43 API Helper', () => {
+    test('getUrl with valid url', () => {
+        expect(false).toBeTruthy();
+    });
+
+    test('getUrl with invalid url', () => {
+        expect(false).toBeTruthy();
+    });
+});
