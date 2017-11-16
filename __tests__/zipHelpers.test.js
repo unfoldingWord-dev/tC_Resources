d540220ca59b5f970caca4da1e52b7316e0db0fd
Bruce McLean <mcleanb@photo-nomad.net>
2017-11-16 10:34:15 -0500

    Fixing Unit tests.

--- __tests__/zipHelpers.test.js
+++ __tests__/zipHelpers.test.js
@@ -1,13 +1,65 @@
+import fs from 'fs-extra';
 import { extractZipFile } from "../helpers/zipHelpers";
 
 describe('Zip Helpers', () => {
+  let zipfilepath = null;
+  let resourceinputpath = null;
 
-    test('extractZipFile with valid input', () => {
-        expect(false).toBeTruthy();
-    });
+  beforeEach(() => {
+    zipfilepath = null;
+    resourceinputpath = null;
+  });
 
-    test('extractZipFile with invalid input', () => {
-        expect(false).toBeTruthy();
-    });
+  afterEach(() => {
+    if (resourceinputpath) {
+      fs.removeSync(resourceinputpath);
+    }
+  });
 
-});
+  test('extractZipFile with valid input', () => {
+    const zipFileName = 'bhp-tit.usfm.zip';
+    zipfilepath = './__tests__/output/' + zipFileName;
+    fs.copySync('./__tests__/fixtures/' + zipFileName, zipfilepath); // copy zip to new location since it will be deleted
+    resourceinputpath = './__tests__/output/dummyDestinationFolder';
+    const expectException = false;
+    const expectedOutputFile = resourceinputpath + '/bhp-tit.usfm';
+
+    runExtractZipFile(zipfilepath, resourceinputpath, expectException);
+    expect(fs.existsSync(expectedOutputFile)).toBeTruthy();
+  });
+
+  test('extractZipFile with dummy names', () => {
+    zipfilepath = './dummySource.zip';
+    const resourceinputpath = './__tests__/output/dummyDestinationFolder';
+    const expectException = true;
+
+    runExtractZipFile(zipfilepath, resourceinputpath, expectException);
+  });
+
+  test('extractZipFile with invalid input', () => {
+    zipfilepath = null;
+    resourceinputpath = null;
+    const expectException = true;
+
+    runExtractZipFile(zipfilepath, resourceinputpath, expectException);
+  });
+
+  //
+  // helpers
+  //
+  function runExtractZipFile(zipfilepath, resourceinputpath, expectException) {
+    let exception = true;
+    try {
+      if(resourceinputpath) {
+        fs.removeSync(resourceinputpath);
+      }
+      extractZipFile(zipfilepath, resourceinputpath);
+      exception = false;
+    } catch (e) {
+      console.log("failure: " + e);
+      exception = true;
+    }
+    expect(exception).toEqual(expectException);
+  }
+
+  });
