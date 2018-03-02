76df1e6b857ec32e4c4f0097be3544e46651eb9e
Bruce McLean <mcleanb@photo-nomad.net>
2018-03-02 11:23:14 -0500

    Trying version v for less than v0

--- __tests__/englishUlbHelpers.test.js
+++ __tests__/englishUlbHelpers.test.js
@@ -13,7 +13,7 @@ describe('englishUlbHelpers', function() {
 
   it('should output ULB chapter files', () => {
     return new Promise((resolve) => {
-      const version = 'v-1';
+      const version = 'v';
       const resourceinputpath = path.join('__tests__','fixtures','bible', 'en_aligned');
 
       const UGNTOutputPath = path.join('__tests__', 'output', 'en-ulb-sources', version);
@@ -29,7 +29,7 @@ describe('englishUlbHelpers', function() {
 
   // it('should download and output en_ULB chapter files', () => {
   //   return new Promise((resolve) => {
-  //     const version = 'v-1';
+  //     const version = 'v';
   //     EnglishUlbHelpers.generateVersion(version, resolve);
   //   }).then(() => {
   //     console.log('en_ulb processing completed!');
