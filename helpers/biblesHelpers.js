eefc9ab58616a3c9ef144834afb7d7f3022fa933
mannycolon <colonmanuel7@gmail.com>
2018-03-15 15:11:21 -0400

    fixed the biblesHelpers' generateBibles()

--- helpers/biblesHelpers.js
+++ helpers/biblesHelpers.js
@@ -4,76 +4,74 @@ import usfm from 'usfm-js';
 import yaml from 'yamljs';
 
 /**
- * 
- * @param {array} bibles 
- * @param {String} extractedFilePath 
- * @param {String} RESOURCE_OUTPUT_PATH 
+ * Generates tC compatible bible data structure.
+ * @param {array} bibles
+ * @param {String} extractedFilePath
+ * @param {String} RESOURCE_OUTPUT_PATH
  */
-export function generateBibles(
-  bibles,
-  extractedFilePath,
-  RESOURCE_OUTPUT_PATH,
-) {
-  console.log(
-    '\x1b[33m%s\x1b[0m',
-    'Generating tC compatible resource data structure ...',
-  );
-  bibles.forEach(bible => {
-    const pathToUsfmFile = path.join(extractedFilePath, bible);
-    let oldManifest = getResourceManifestFromYaml(extractedFilePath);
-    let bibleVersion = 'v' + oldManifest.dublin_core.version;
-    generateBibleManifest(oldManifest, bibleVersion, RESOURCE_OUTPUT_PATH);
+export function generateBibles(bibles, extractedFilePath, RESOURCE_OUTPUT_PATH) {
+  try {
+    console.log(
+      '\x1b[33m%s\x1b[0m',
+      'Generating tC compatible bible resource data structure ...',
+    );
+    bibles.forEach(bible => {
+      const pathToUsfmFile = path.join(extractedFilePath, bible);
+      let oldManifest = getResourceManifestFromYaml(extractedFilePath);
+      let bibleVersion = 'v' + oldManifest.dublin_core.version;
+      generateBibleManifest(oldManifest, bibleVersion, RESOURCE_OUTPUT_PATH);
 
-    let usfmBibleBook = fs.readFileSync(pathToUsfmFile).toString('utf8');
-    console.log(extractedFilePath + '/n.json');
-    fs.outputJsonSync(extractedFilePath + '/aa.json', usfm.toJSON(usfmBibleBook));
-    let jsonBibleBook = usfm.toJSON(usfmBibleBook);
-    console.log(jsonBibleBook);
+      let usfmBibleBook = fs.readFileSync(pathToUsfmFile).toString('utf8');
+      fs.outputJsonSync(extractedFilePath + '/aa.json', usfm.toJSON(usfmBibleBook));
+      let jsonBibleBook = usfm.toJSON(usfmBibleBook);
 
-    // get parsed book
-    const chapters = Object.keys(jsonBibleBook);
-    chapters.forEach(chapterNumber => {
-      let arrayValue = parseInt(chapterNumber, 10);
-      // only allow chapter numbers to generate bible (no strings)
-      if (typeof arrayValue === 'number' && !isNaN(arrayValue)) {
-        let fileName = chapterNumber + '.json';
-        let bibleId = jsonBibleBook.headers['toc3']
-          .toLowerCase()
-          .replace(' ', '');
-        console.log(bibleId);
-        let savePath = path.join(
-          RESOURCE_OUTPUT_PATH,
-          bibleVersion,
-          bibleId,
-          fileName,
-        );
-        fs.outputJsonSync(savePath, jsonBibleBook[chapterNumber]);
-      }
+      // get parsed book
+      const chapters = Object.keys(jsonBibleBook.chapters);
+      chapters.forEach(chapterNumber => {
+        let arrayValue = parseInt(chapterNumber, 10);
+        // only allow chapter numbers to generate bible (no strings)
+        if (typeof arrayValue === 'number' && !isNaN(arrayValue)) {
+
+          let fileName = chapterNumber + '.json';
+          const toc3Header = jsonBibleBook.headers.find((header) => header.tag === 'toc3');
+          let bibleId = toc3Header.content.toLowerCase().replace(' ', '');
+          console.log('\x1b[33m%s\x1b[0m', `bibleId:${bibleId}`);
+          let savePath = path.join(
+            RESOURCE_OUTPUT_PATH,
+            bibleVersion,
+            bibleId,
+            fileName,
+          );
+          fs.outputJsonSync(savePath, jsonBibleBook[chapterNumber]);
+        }
+      });
     });
-  });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /**
- * 
- * @param {String} extractedFilePath 
+ *
+ * @param {String} extractedFilePath
  */
 export function getResourceManifestFromYaml(extractedFilePath) {
-  let filePath = path.join(extractedFilePath, 'manifest.yaml');
-  let yamlManifest = fs.readFileSync(filePath, 'utf8');
-  return yaml.parse(yamlManifest);
+  try {
+    const filePath = path.join(extractedFilePath, 'manifest.yaml');
+    const yamlManifest = fs.readFileSync(filePath, 'utf8');
+    return yaml.parse(yamlManifest);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /**
- * 
- * @param {object} oldManifest 
- * @param {String} bibleVersion 
- * @param {String} RESOURCE_OUTPUT_PATH 
+ *
+ * @param {object} oldManifest
+ * @param {String} bibleVersion
+ * @param {String} RESOURCE_OUTPUT_PATH
  */
-function generateBibleManifest(
-  oldManifest,
-  bibleVersion,
-  RESOURCE_OUTPUT_PATH,
-) {
+function generateBibleManifest(oldManifest, bibleVersion, RESOURCE_OUTPUT_PATH) {
   let newManifest = {};
   newManifest.language_id = oldManifest.dublin_core.language.identifier;
   newManifest.language_name = oldManifest.dublin_core.language.title;
